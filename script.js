c3b85b392e457b9e0c7e0553509243e46f4aae84
Sugandh-vI <sugandhkr16@gmail.com>
2025-12-05 17:51:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=277b128306366e61d1bc4fbacd4a65b7&page=1';
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=277b128306366e61d1bc4fbacd4a65b7&query=";
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+
+returnMovies(APILINK)
+function returnMovies(url){
+    fetch(url).then(res => res.json())
+    .then(function(data) {
+        console.log(data.results);
+        data.results.forEach(element => {
+            const div_card = document.createElement('div');
+            div_card.setAttribute('class','card');
+
+            const div_column = document.createElement('div');
+            div_column.setAttribute('class','column');
+
+            const div_row = document.createElement('div');
+            div_row.setAttribute('class','row');
+
+            const image = document.createElement('img');
+            image.setAttribute('class','thumbnail');
+            image.setAttribute('id','image');
+
+            const title = document.createElement('h3');
+            title.setAttribute('id','title');
+
+            const center = document.createElement('center'); 
+
+            title.innerHTML = `${element.title}`;
+            image.src = IMG_PATH + element.poster_path;
+
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+
+            main.appendChild(div_row);
+        });
+    });
+}
+
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+
+    main.innerHTML = ''; // when smthng is searched , we need to clear the previous cards , so main is cleared
+
+    const srchitem = search.value;
+
+    if(srchitem){
+        returnMovies(SEARCHAPI+srchitem);
+        search.value = "";
+
+    }
+});
